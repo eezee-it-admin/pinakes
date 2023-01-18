9f29645464d0c5eda7ca8d227e37d80586812125
Sudhir Arya <sudhir@erpharbor.com>
2023-01-18 13:24:40 +0530

    [IMP] Added server action to Confirm the SO and Create the Invoice.

--- addons/pinakes_sales/models/sales_order.py
+++ addons/pinakes_sales/models/sales_order.py
@@ -8,6 +8,25 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _send_order_confirmation_mail(self):
-        if self.sale_order_template_id and self.sale_order_template_id.mail_template_id:
+        so_template = self.sale_order_template_id
+        if so_template and so_template.mail_template_id:
             return
         return super(SaleOrder, self)._send_order_confirmation_mail()
+
+    def action_automate_weborders(self):
+        for order in self:
+            # Confirm the sales order
+            order.action_confirm()
+            # Create invoice
+            order.create_and_validate_invoice()
+        return True
+
+    def create_and_validate_invoice(self):
+        """Create invoice based on delivered qty"""
+        wiz_inv = self.env["sale.advance.payment.inv"].create({})
+        wiz_inv.with_context(
+            active_ids=self.ids, active_model="sale.order"
+        ).create_invoices()
+        # Validate the invoice
+#        self.invoice_ids.with_company(self.company_id).action_post()
+        return True
