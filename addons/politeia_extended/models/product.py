3f331490896649102d352b0a974a8346ca72fde6
Sudhir Arya <sudhir@erpharbor.com>
2023-01-19 09:45:36 +0000

    [2701] Added new master tables with access rights and fields in product

--- addons/politeia_extended/models/product.py
+++ addons/politeia_extended/models/product.py
@@ -1,10 +1,15 @@
 # Copyright 2021      Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    publication_type_id = fields.Many2many('publication.type')
+    organization_id = fields.Many2one('product.organization')
+    product_type_id = fields.Many2one('product.type', 'Type')
+    imprint_id = fields.Many2one('product.imprint')
 
     def action_view_authors(self):
         self.ensure_one()
@@ -16,3 +21,31 @@ class ProductTemplate(models.Model):
             'default_product_tmpl_id': self.id,
         }
         return action
+
+
+class PublicationType(models.Model):
+    _name = 'publication.type'
+    _description = 'Publication Type'
+
+    name = fields.Char(required=True, translate=True)
+
+
+class ProductOrganization(models.Model):
+    _name = 'product.organization'
+    _description = 'Organization'
+
+    name = fields.Char(required=True, translate=True)
+
+
+class ProductType(models.Model):
+    _name = 'product.type'
+    _description = 'Type'
+
+    name = fields.Char(required=True, translate=True)
+
+
+class ProductImprint(models.Model):
+    _name = 'product.imprint'
+    _description = 'Imprint'
+
+    name = fields.Char(required=True, translate=True)
