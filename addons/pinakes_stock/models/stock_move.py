221bf120d1a172a5aca28397da71c4db5d4d9aa2
Hafedh RAHMANI <hafedh.rahmani@eezee-it.com>
2023-01-20 13:56:53 +0100

    automatic export records

--- addons/pinakes_stock/models/stock_move.py
+++ addons/pinakes_stock/models/stock_move.py
@@ -1,14 +1,9 @@
 # Copyright 2023 Eezee-IT (<https://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-import base64
-import datetime
 import logging
-import re
-from io import BytesIO
-import pandas as pd
+from odoo import fields, models
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
--- addons/pinakes_stock/models/stock_picking.py
+++ addons/pinakes_stock/models/stock_picking.py
@@ -1,14 +1,8 @@
 # Copyright 2023 Eezee-IT (<https://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-import base64
-import datetime
 import logging
-import re
-from io import BytesIO
-import pandas as pd
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from odoo import api, fields, models
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
