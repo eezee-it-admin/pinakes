c5020069ab55890fb9d96a31a72c7f5781032b88
Hafedh RAHMANI <hafedh.rahmani@eezee-it.com>
2023-01-20 14:59:54 +0100

    automatic export conditions

--- addons/pinakes_stock/__manifest__.py
+++ addons/pinakes_stock/__manifest__.py
@@ -1,23 +1,6 @@
-# -*- coding: utf-8 -*-
-# #############################################################################
-#
-#    Copyright Eezee-It (C) 2017-2020
-#    Author: Eezee-It <info@eezee-it.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright 2023 Eezee-IT (<https://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 {
     "name": "Pinakes stock",
     "version": '15.0.0.0.0',
