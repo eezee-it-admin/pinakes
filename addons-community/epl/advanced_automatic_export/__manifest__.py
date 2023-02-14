bbdcb65debbb9f2945ddaabbb2196a7a2a60ac9d
faten-ezee <faten.gabouch@eezee-it.com>
2023-02-14 15:36:19 +0100

    update

--- addons-community/epl/advanced_automatic_export/__manifest__.py
+++ addons-community/epl/advanced_automatic_export/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Automatic Export',
     'summary': "Generate a file that can be send to other application.",
-    'version': '16.0.1.0.1',
+    'version': '16.0.0.0.0',
     'author': "Eezee-It",
     'license': "AGPL-3",
     'category': 'Extra Tools',
