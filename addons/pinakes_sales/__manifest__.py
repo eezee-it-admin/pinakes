b9ef12a2fb9fcdc2e2a97729cdb0e9f45cef730d
Keval <keval.mehta@eezee-it.com>
2023-05-31 20:26:22 +0530

    [UPDATE] Name change of button

--- addons/pinakes_sales/__manifest__.py
+++ addons/pinakes_sales/__manifest__.py
@@ -2,12 +2,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Pinakes Sale',
-    'version': '16.0.0.0.1',
+    'version': '16.0.0.0.2',
     'author': 'Eezee-It',
     'category': 'Sale',
     'license': 'LGPL-3',
     'depends': [
         'sale',
+        'sale_subscription'
     ],
     'data': [
         'data/server_action.xml',
