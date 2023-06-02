a00d2e3dc985befdfc535df0abeeddf9f1a64904
Keval <keval.mehta@eezee-it.com>
2023-06-02 14:33:10 +0530

    [FIX] Pipeline

--- addons/pinakes_base/models/product.py
+++ addons/pinakes_base/models/product.py
@@ -84,9 +84,9 @@ class ProductProduct(models.Model):
                                       ('product', 'Storable Product')],
                                      string='Product Type', default='consu',
                                      required=True, help='A storable product is a product for which you manage stock. '
-                                                         'The Inventory app has to be installed.\n A consumable product is a'
-                                                         ' product for which stock is not managed.\n A service is a '
-                                                         'non-material product you provide.')
+                                                         'The Inventory app has to be installed.\n A consumable '
+                                                         'product is a product for which stock is not managed.\n '
+                                                         'A service is a non-material product you provide.')
     type = fields.Selection([('consu', 'Consumable'), ('service', 'Service'),
                              ('product', 'Storable Product')],
                             compute='_compute_type', store=True,
