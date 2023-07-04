b1a4b5d45c525f31e294ffe67de3cf949da2f156
Keval Mehta <keval.m.erpharbor@gmail.com>
2023-07-04 09:02:57 +0000

    [31329] Add sales filter in report

--- addons/pinakes_sales/__manifest__.py
+++ addons/pinakes_sales/__manifest__.py
@@ -2,16 +2,18 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Pinakes Sale',
-    'version': '16.0.0.0.2',
+    'version': '16.0.0.0.3',
     'author': 'Eezee-It',
     'category': 'Sale',
     'license': 'LGPL-3',
     'depends': [
-        'sale_subscription'
+        'sale_subscription',
+        'pinakes_base'
     ],
     'data': [
         'data/server_action.xml',
         'views/sale_order.xml',
-        'views/sale_order_recurrence.xml'
+        'views/sale_order_recurrence.xml',
+        'report/sale_report.xml'
     ],
 }
