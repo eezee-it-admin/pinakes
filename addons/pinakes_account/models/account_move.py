0bc3a2ded8390952e383675f8653c3b6809b035f
“Anaghan <hiren.anaghan@eezee-it.com>
2023-10-16 14:48:15 +0530

    [33039] Email Templates per company

--- addons/pinakes_account/models/account_move.py
+++ addons/pinakes_account/models/account_move.py
@@ -8,14 +8,23 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    # Inherited to select default email template
-    def _get_mail_template(self):
+    # # Inherited to select default email template
+    # def _get_mail_template(self):
+    #     if not all(move.move_type == 'out_refund' for move in self):
+    #         email_template = self.env['mail.template'].search(
+    #             [('default_template', '=', True)])
+    #         if email_template:
+    #             return str(email_template.id) #email_template.get_external_id().get(email_template.id)
+    #         else:
+    #             return super()._get_mail_template()
+    #     else:
+    #         return super()._get_mail_template()
+
+    def action_invoice_sent(self):
+        res = super(AccountMove, self).action_invoice_sent()
         if not all(move.move_type == 'out_refund' for move in self):
-            email_template = self.env['mail.template'].search(
-                [('default_template', '=', True)])
-            if email_template:
-                return email_template.get_external_id().get(email_template.id)
-            else:
-                return super()._get_mail_template()
-        else:
-            return super()._get_mail_template()
+            template = self.env['mail.template'].search(
+                [('default_template', '=', True)], limit=1)
+            if template:
+                res['context'].update({'default_template_id': template.id})
+        return res
