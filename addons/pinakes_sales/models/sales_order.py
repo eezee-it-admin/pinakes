4e690ebb16a54e4c03c9d909c3556cb89f4f1924
Hiren Anaghan <hiren.anaghan@eezee-it.com>
2023-11-02 13:44:25 +0000

    Heri/ch/33395

--- addons/pinakes_sales/models/sales_order.py
+++ addons/pinakes_sales/models/sales_order.py
@@ -30,6 +30,23 @@ class SaleOrder(models.Model):
         wiz_inv.with_context(
             active_ids=self.ids, active_model="sale.order"
         ).create_invoices()
-        # Validate the invoice
-#        self.invoice_ids.with_company(self.company_id).action_post()
         return True
+
+    """We need to remove the section line[Order subscriptions recurrence line]
+        because the invoice does not require it. We have already
+        completed the product line, so there is no need for an
+        additional line for the invoice or report"""
+    def _get_invoiceable_lines(self, final=False):
+        res = super()._get_invoiceable_lines(final=final)
+        is_99 = False
+        value = ['99 jaren', '99 Jaren']
+        for rec in self:
+            if rec.recurrence_id and\
+                    rec.recurrence_id.name in value:
+                is_99 = True
+        if is_99:
+            return res.\
+                filtered(lambda x: x.display_type != 'line_note'
+                         and x.sequence != '999')
+        else:
+            return res
