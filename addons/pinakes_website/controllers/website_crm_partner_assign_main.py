105c154dc8814cc49b46419f8ce1cde7c35233ab
eezeeadmin <xboulite020@hotmail.com>
2023-11-29 12:01:03 +0100

    Task 37087  Auteurs logic step 2 => copy paste of the standard function, otherwise we loose some logic ...

--- addons/pinakes_website/controllers/website_crm_partner_assign_main.py
+++ addons/pinakes_website/controllers/website_crm_partner_assign_main.py
@@ -2,41 +2,117 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.addons.website_crm_partner_assign.controllers.main import WebsiteCrmPartnerAssign
+import werkzeug.urls
+from werkzeug.exceptions import NotFound
 
 from odoo import http
 from odoo.http import request
+from odoo.addons.http_routing.models.ir_http import slug, unslug
+
+from odoo.tools.translate import _
 
 
 class CustomWebsiteCrmPartnerAssign(WebsiteCrmPartnerAssign):
 
     def partners(self, country=None, grade=None, page=0, **post):
-        # Call the super method and get its output
-        response = super(CustomWebsiteCrmPartnerAssign, self).partners(country, grade, page, **post)
+        country_all = post.pop('country_all', False)
+        partner_obj = request.env['res.partner']
+        country_obj = request.env['res.country']
+        search = post.get('search', '')
 
-        # Define a new base_partner_domain without the 'is_company' condition
+        # Same code copy pasted for the whole function, we just have removed the ('is_company', '=', True)
+        # domain filter
         base_partner_domain = [('grade_id', '!=', False), ('website_published', '=', True)]
         if not request.env['res.users'].has_group('website.group_website_restricted_editor'):
             base_partner_domain += [('grade_id.website_published', '=', True)]
-        if response.qcontext.get('search'):
-            search = response.qcontext.get('search')
+        if search:
             base_partner_domain += ['|', ('name', 'ilike', search), ('website_description', 'ilike', search)]
 
-        # Apply additional domain filters based on country and grade, if present
+        # group by grade
+        grade_domain = list(base_partner_domain)
+        if not country and not country_all:
+            country_code = request.geoip.get('country_code')
+            if country_code:
+                country = country_obj.search([('code', '=', country_code)], limit=1)
         if country:
-            base_partner_domain += [('country_id', '=', country.id)]
+            grade_domain += [('country_id', '=', country.id)]
+        grades = partner_obj.sudo().read_group(
+            grade_domain, ["id", "grade_id"],
+            groupby="grade_id")
+        grades_partners = partner_obj.sudo().search_count(grade_domain)
+        # flag active grade
+        for grade_dict in grades:
+            grade_dict['active'] = grade and grade_dict['grade_id'][0] == grade.id
+        grades.insert(0, {
+            'grade_id_count': grades_partners,
+            'grade_id': (0, _("All Categories")),
+            'active': bool(grade is None),
+        })
+
+        # group by country
+        country_domain = list(base_partner_domain)
+        if grade:
+            country_domain += [('grade_id', '=', grade.id)]
+        countries = partner_obj.sudo().read_group(
+            country_domain, ["id", "country_id"],
+            groupby="country_id", orderby="country_id")
+        countries_partners = partner_obj.sudo().search_count(country_domain)
+        # flag active country
+        for country_dict in countries:
+            country_dict['active'] = country and country_dict['country_id'] and country_dict['country_id'][
+                0] == country.id
+        countries.insert(0, {
+            'country_id_count': countries_partners,
+            'country_id': (0, _("All Countries")),
+            'active': bool(country is None),
+        })
+
+        # current search
         if grade:
             base_partner_domain += [('grade_id', '=', grade.id)]
+        if country:
+            base_partner_domain += [('country_id', '=', country.id)]
 
-        # Perform a search with the updated domain
-        partner_obj = request.env['res.partner']
+        # format pager
+        if grade and not country:
+            url = '/partners/grade/' + slug(grade)
+        elif country and not grade:
+            url = '/partners/country/' + slug(country)
+        elif country and grade:
+            url = '/partners/grade/' + slug(grade) + '/country/' + slug(country)
+        else:
+            url = '/partners'
+        url_args = {}
+        if search:
+            url_args['search'] = search
+        if country_all:
+            url_args['country_all'] = True
+
+        partner_count = partner_obj.sudo().search_count(base_partner_domain)
+        pager = request.website.pager(
+            url=url, total=partner_count, page=page, step=self._references_per_page, scope=7,
+            url_args=url_args)
+
+        # search partners matching current search parameters
         partner_ids = partner_obj.sudo().search(
-            base_partner_domain,
-            order="grade_sequence ASC, implemented_partner_count DESC, display_name ASC, id ASC",
-            offset=response.qcontext.get('pager')['offset'],
-            limit=self._references_per_page
-        )
+            base_partner_domain, order="grade_sequence ASC, implemented_partner_count DESC, display_name ASC, id ASC",
+            offset=pager['offset'], limit=self._references_per_page)
+        partners = partner_ids.sudo()
 
-        # Update the response context with the new partners list
-        response.qcontext['partners'] = partner_ids
+        google_map_partner_ids = ','.join(str(p.id) for p in partners)
+        google_maps_api_key = request.website.google_maps_api_key
 
-        return response
+        values = {
+            'countries': countries,
+            'country_all': country_all,
+            'current_country': country,
+            'grades': grades,
+            'current_grade': grade,
+            'partners': partners,
+            'google_map_partner_ids': google_map_partner_ids,
+            'pager': pager,
+            'searches': post,
+            'search_path': "%s" % werkzeug.urls.url_encode(post),
+            'google_maps_api_key': google_maps_api_key,
+        }
+        return request.render("website_crm_partner_assign.index", values, status=partners and 200 or 404)
