11f03229bb20eff9250a8582aa2d51b0eb1d539d
eezeeadmin <xboulite020@hotmail.com>
2023-11-30 18:33:04 +0100

    Task 37087 - authors_controller deleted, as we only use the /partners route

--- addons/pinakes_website/controllers/__init__.py
+++ addons/pinakes_website/controllers/__init__.py
@@ -2,4 +2,3 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import website_crm_partner_assign_main
 from . import website_partner
-# from . import authors_controller
