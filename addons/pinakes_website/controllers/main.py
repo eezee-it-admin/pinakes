52b0c3b333b271d8cfffc4cc0b8f111d8b8467e1
eezeeadmin <xboulite020@hotmail.com>
2023-12-08 09:50:37 +0100

    Task 37094 :  Search bar , new attempt using get_search_domain

--- addons/pinakes_website/controllers/main.py
+++ addons/pinakes_website/controllers/main.py
@@ -5,9 +5,11 @@ from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.osv import expression
 
+import pprint
+
 
 class CustomShopController(WebsiteSale):
-    @http.route()  # Use appropriate route decorators as required
+
     def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
         domains = [request.website.sale_product_domain()]
         search_type = request.httprequest.args.get('search_type')
@@ -16,29 +18,27 @@ class CustomShopController(WebsiteSale):
             for srch in search.split(" "):
                 subdomains = [
                     [('name', 'ilike', srch)],
-                    [('product_variant_ids.default_code', 'ilike', srch)]
+                    [('product_variant_ids.default_code', 'ilike', srch)],
+                    [('product_variant_ids.isbn', 'ilike', srch)]
                 ]
                 if search_in_description:
                     subdomains.append([('website_description', 'ilike', srch)])
                     subdomains.append([('description_sale', 'ilike', srch)])
 
-                # Custom domain logic
-                if search_type == 'products_only':
-                    matching_variants = self.env['product.product'].search([('isbn', 'ilike', srch)])
-                    matching_templates_ids = matching_variants.mapped('product_tmpl_id').ids
-                    author_products = self.env['product.author'].search([('partner_id.name', 'ilike', srch)]).mapped(
-                        'product_tmpl_id')
+                # if search_type == 'products_only':
+                #     matching_variants = self.env['product.product'].search([('isbn', 'ilike', srch)])
+                #     matching_templates_ids = matching_variants.mapped('product_tmpl_id').ids
+                #     author_products = self.env['product.author'].search([('partner_id.name', 'ilike', srch)]).mapped(
+                #         'product_tmpl_id')
+                #
+                #     matching_isbn_domain = [('id', 'in', matching_templates_ids)]
+                #     matching_author_domain = [('id', 'in', author_products.ids)]
+                #     tag_domain = [('product_tag_ids.name', 'ilike', srch)]
+                #
+                #     subdomains.append(matching_isbn_domain)
+                #     subdomains.append(matching_author_domain)
+                #     subdomains.append(tag_domain)
 
-                    matching_isbn_domain = [('id', 'in', matching_templates_ids)]
-                    matching_author_domain = [('id', 'in', author_products.ids)]
-                    tag_domain = [('product_tag_ids.name', 'ilike', srch)]
-
-                    # Append custom domains individually
-                    subdomains.append(matching_isbn_domain)
-                    subdomains.append(matching_author_domain)
-                    subdomains.append(tag_domain)
-
-                # Combine subdomains using OR and append to main domains
                 domains.append(expression.OR(subdomains))
 
         if category:
@@ -59,6 +59,9 @@ class CustomShopController(WebsiteSale):
                     ids = [value[1]]
             if attrib:
                 domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+
+        pprint.pprint(domains)
+        pprint.pprint(request.env["product.template"].search(expression.AND(domains)))
 
         return expression.AND(domains)
 
