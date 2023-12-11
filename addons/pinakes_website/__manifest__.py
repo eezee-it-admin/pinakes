f5aa52aaa7054487164460e875b04e7ad35eceb2
eezeeadmin <xboulite020@hotmail.com>
2023-12-11 14:29:05 +0100

    Task 37115 : fixing this branch

--- addons/pinakes_website/__manifest__.py
+++ addons/pinakes_website/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2023      Eezee-IT (<http://www.eezee-it.com>)
+# Copyright 2023  Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Pinakes Website",
-    "version": "16.0.0.0.1",
+    "version": "16.0.0.0.2",
     "author": "Eezee-It",
     "category": "Website",
     "license": "LGPL-3",
@@ -11,16 +11,23 @@
         "website",
         "product",
         "pinakes_base",
+        "website_crm_partner_assign",
         "website_sale",
         "website_sale_wishlist",
     ],
     "data": [
+        # "views/website_templates.xml",
+        # "views/snippets/dynamic_snippets/s_product_public_category_tags.xml",
+        # "views/snippets.xml",
+        "views/website_templates.xml",
+        "views/website_crm_partner_assign_templates.xml",
         "templates/wsale_product.xml",
     ],
     'assets': {
         'web.assets_frontend': [
             'pinakes_website/static/src/js/publication_redirect.js',
             'pinakes_website/static/src/js/tag_filters_checkboxed.js',
+            'pinakes_website/static/src/js/website_reseller_template.js',
         ],
     },
     "application": False,
--- addons/pinakes_website/controllers/main.py
+++ addons/pinakes_website/controllers/main.py
@@ -1,7 +1,5 @@
 # Copyright 2023      Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import http
-from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
@@ -10,15 +8,13 @@ class CustomShopController(WebsiteSale):
         self, category=None, attrib_values=None, pricelist=None,
         min_price=0.0, max_price=0.0, conversion_rate=1, **post
     ):
-        # Call the super method to get the base options
         options = super(CustomShopController, self)._get_search_options(
             category, attrib_values, pricelist, min_price, max_price,
             conversion_rate, **post
         )
 
-        # Add tag_ids to the options
         tag_ids = post.get('tag')
-        pprint.pprint(tag_ids)
+
         if tag_ids:
             options['product_tag_ids'] = [int(tag_id) for tag_id in tag_ids.split(',')]
 
