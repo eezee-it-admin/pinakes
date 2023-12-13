aeba8ab5abbd4cd5a16fe42dbb65659f3cb79a4c
Jordan Grauwmans <jordan.grauwmans@eezee-it.com>
2023-12-13 09:43:41 +0000

    Task 37115 : gerelateerde tags

--- addons/pinakes_website/__manifest__.py
+++ addons/pinakes_website/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2023      Eezee-IT (<http://www.eezee-it.com>)
+# Copyright 2023  Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Pinakes Website",
@@ -9,16 +9,22 @@
     "summary": "Pinakes Website",
     "depends": [
         "website",
+        "product",
         "pinakes_base",
         "website_crm_partner_assign",
+        "website_sale",
+        "website_sale_wishlist",
     ],
     "data": [
         "views/website_partner_templates.xml",
         "views/website_templates.xml",
-        "views/website_crm_partner_assign_templates.xml"
+        "views/website_crm_partner_assign_templates.xml",
+        "templates/wsale_product.xml",
     ],
     'assets': {
         'web.assets_frontend': [
+            'pinakes_website/static/src/js/publication_redirect.js',
+            'pinakes_website/static/src/js/tag_filters_checkboxed.js',
             'pinakes_website/static/src/js/website_reseller_template.js',
             'pinakes_website/static/src/js/publication_redirect.js'
         ],
