6ff9be863314de601e9a6b0110a644747367b54e
jordan <jordan.grauwmans@eezee-it.com>
2023-12-14 22:02:02 +0100

    BooXtream API link

---
+++ addons/pinakes_api/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Eezee-IT (<http://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Pinakes API",
+    "version": "16.0.0.0.1",
+    "author": "Eezee-It",
+    "category": "Services",
+    "license": "LGPL-3",
+    "summary": "Pinakes API",
+    "depends": [
+        "base",
+        "sale",
+        "website_sale",
+
+        "pinakes_mass_mailing",
+    ],
+    'data': [
+        # views
+        'views/product_template_view.xml',
+    ],
+    "application": False,
+}
