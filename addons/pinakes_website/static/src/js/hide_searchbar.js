017f0d343e09974a70210a06e555cca8e20f4ec0
eezeeadmin <xboulite020@hotmail.com>
2023-12-27 10:13:20 +0100

    Task 38455 - Last touch to apply to a custom inner searchbar, and to hook it at the "init" method in JS

--- addons/pinakes_website/static/src/js/hide_searchbar.js
+++ addons/pinakes_website/static/src/js/hide_searchbar.js
@@ -1,10 +1,10 @@
-odoo.define('pinakes_website.custom_script', function(require) {
+odoo.define('pinakes_website.hide_searchbar_script', function(require) {
     'use strict';
 
     var publicWidget = require('web.public.widget');
 
     publicWidget.RootWidget.include({
-        start: function() {
+        init: function(parent) {
             this._super.apply(this, arguments);
             if (window.location.pathname.startsWith('/shop')) {
                 $('body').addClass('shop-page');
@@ -12,3 +12,4 @@ odoo.define('pinakes_website.custom_script', function(require) {
         },
     });
 });
+
