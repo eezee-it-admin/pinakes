25ef927772477d344bbc7d25eb0f1141812f1ffc
Ivan Chorine <ivan.chorine@gmail.com>
2023-12-28 10:35:21 +0000

    Commit to make the script apply when another language is chosen

--- addons/pinakes_website/static/src/js/hide_searchbar.js
+++ addons/pinakes_website/static/src/js/hide_searchbar.js
@@ -7,7 +7,11 @@ odoo.define('pinakes_website.hide_searchbar_script', function(require) {
         init: function(parent) {
             this._super.apply(this, arguments);
             var path = window.location.pathname;
-            if (path === '/shop' || path.startsWith('/shop?') || path.startsWith('/shop/category')) {
+
+            if (path === '/shop' || path.startsWith('/shop?') || path.startsWith('/shop/category') ||
+                path === '/fr_BE/shop' || path.startsWith('/fr_BE/shop?') || path.startsWith('/fr_BE/shop/category') ||
+                path === '/en_UK/shop' || path.startsWith('/en_UK/shop?') || path.startsWith('/en_UK/shop/category') ||
+                path === '/en_US/shop' || path.startsWith('/en_US/shop?') || path.startsWith('/en_US/shop/category')) {
                 $('body').addClass('shop-page');
             }
         },
