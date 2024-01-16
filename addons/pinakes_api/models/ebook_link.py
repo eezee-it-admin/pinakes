57e97d9c6277b3c6609e507afcd2034dc5bc584d
“Anaghan <hiren.anaghan@eezee-it.com>
2024-01-16 17:01:29 +0530

    Fixed : Duplicate Generate value

--- addons/pinakes_api/models/ebook_link.py
+++ addons/pinakes_api/models/ebook_link.py
@@ -5,5 +5,6 @@ class BooxtreamLink(models.Model):
     _name = "ebook.link"
 
     sale_order_id = fields.Many2one('sale.order')
+    product_template_id = fields.Many2one('product.template')
     download_link = fields.Char()
     read_link = fields.Char()
