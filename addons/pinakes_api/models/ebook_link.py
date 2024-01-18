8998d61e64b234dc9127154db56880af7bc04694
“Anaghan <hiren.anaghan@eezee-it.com>
2024-01-18 15:40:37 +0530

    [39195] Warning to be fixed

--- addons/pinakes_api/models/ebook_link.py
+++ addons/pinakes_api/models/ebook_link.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 
 class BooxtreamLink(models.Model):
     _name = "ebook.link"
+    _description = 'eBook Link'
 
     sale_order_id = fields.Many2one('sale.order')
     product_template_id = fields.Many2one('product.template')
