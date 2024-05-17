7d77e5b5a87b0b6e6395e68cbecb1438841a84df
islem <islem.namouchi@eezee-it.com>
2024-05-17 08:40:04 +0100

    add payment provider to abonnement

---
+++ addons/pinakes_account/models/payment_provider.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Eezee-IT (<http://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models, api
+from odoo.osv import expression
+
+
+class PaymentProvider(models.Model):
+    _inherit = 'payment.provider'
+    @api.model
+    def _get_compatible_providers(
+        self, company_id, partner_id, amount, currency_id=None, force_tokenization=False,
+        is_express_checkout=False, is_validation=False, **kwargs
+    ):
+        """ Select and return the providers matching the criteria.
+
+        The criteria are that providers must not be disabled, be in the company that is provided,
+        and support the country of the partner if it exists. The criteria can be further refined
+        by providing the keyword arguments.
+
+        :param int company_id: The company to which providers must belong, as a `res.company` id.
+        :param int partner_id: The partner making the payment, as a `res.partner` id.
+        :param float amount: The amount to pay. `0` for validation transactions.
+        :param int currency_id: The payment currency, if known beforehand, as a `res.currency` id.
+        :param bool force_tokenization: Whether only providers allowing tokenization can be matched.
+        :param bool is_express_checkout: Whether the payment is made through express checkout.
+        :param bool is_validation: Whether the operation is a validation.
+        :param dict kwargs: Optional data. This parameter is not used here.
+        :return: The compatible providers.
+        :rtype: recordset of `payment.provider`
+        """
+        # Compute the base domain for compatible providers.
+        domain = ['&', ('state', 'in', ['enabled', 'test']), ('company_id', '=', company_id)]
+
+        # Handle the is_published state.
+        if not self.env.user._is_internal():
+            domain = expression.AND([domain, [('is_published', '=', True)]])
+
+        # Handle partner country.
+        partner = self.env['res.partner'].browse(partner_id)
+        if partner.country_id:  # The partner country must either not be set or be supported.
+            domain = expression.AND([
+                domain, [
+                    '|',
+                    ('available_country_ids', '=', False),
+                    ('available_country_ids', 'in', [partner.country_id.id]),
+                ]
+            ])
+
+        # Handle the maximum amount.
+        currency = self.env['res.currency'].browse(currency_id).exists()
+        if not is_validation and currency:  # The currency is required to convert the amount.
+            company = self.env['res.company'].browse(company_id).exists()
+            date = fields.Date.context_today(self)
+            converted_amount = currency._convert(amount, company.currency_id, company, date)
+            domain = expression.AND([
+                domain, [
+                    '|', '|',
+                    ('maximum_amount', '>=', converted_amount),
+                    ('maximum_amount', '=', False),
+                    ('maximum_amount', '=', 0.),
+                ]
+            ])
+
+        # Handle tokenization support requirements.
+        if force_tokenization or self._is_tokenization_required(**kwargs):
+            domain = expression.AND([domain, [('allow_tokenization', '=', True)]])
+
+        # Handle express checkout.
+        if is_express_checkout:
+            domain = expression.AND([domain, [('allow_express_checkout', '=', True)]])
+
+        compatible_providers = self.env['payment.provider'].search(domain)
+        return compatible_providers
